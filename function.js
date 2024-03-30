02f6082c468269a0b1abecad2a056a85203b9258
Blaze114 <129754384+Blaze114@users.noreply.github.com>
2024-03-30 21:14:17 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function tillbaka() {
+    window.location.href = "index.html"
+}
+
+function inställningar() {
+    window.location.href = "inställningar.html"
+}
+
+function se_registrerade() {
+    window.location.href = "se_registrerade_användare.html"
+}
